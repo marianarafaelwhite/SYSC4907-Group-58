eb52e4ef71f7b5d0a186d08a170cf6349e7a6549
Mariana Rafael-White <marianarafaelwhite@cmail.carleton.ca>
2021-02-13 16:02:06 -0800

    Update IoT Forwarder to support diff ports
    This commit adds command line arguments for the
    iot_forwarder.py script to accept different port numbers:
    one port number for the socket to receive messages on,
    another port number for the socket to send to.
    
    This is to accomodate running with the actual hardware:
    
    1) First, in Linux:
    python3 iot_forwarder.py -ip 192.168.49.2 -r 7777 -s <PORT>
        (<PORT> = Port of classifier)
    
    2) Second, in Mac OS x:
    python3 iot_forwarder.py -ip 10.211.55.4 -r 7777 -s 7777
    
    3) Finally, on pi:
    ./hardware.py -ip 10.0.0.200 -v -p 7777

--- iot/iot_forwarder.py
+++ iot/iot_forwarder.py
@@ -2,23 +2,31 @@ import argparse
 import socket
 
 IP = "10.211.55.4"
-UDP_PORT = 7777
+SEND_PORT = 7777
+RECEIVE_PORT = 7777
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-i", help="specify a host, default is 10.211.55.4")
-parser.add_argument("-p", help="specify a port, default is 7777")
+parser.add_argument("-v", action='store_true', help="print debug messages")
+parser.add_argument(
+    "-ip",
+    help="specify a host (e.g., 192.168.49.2), default is 10.211.55.4")
+parser.add_argument("-s", help="specify a port to send to, default is 7777")
+parser.add_argument("-r", help="specify a port to receive on, default is 7777")
 args = parser.parse_args()
-if args.i:
-    IP = args.i
-if args.p:
-    UDP_PORT = args.p
+if args.ip:
+    IP = args.ip
+if args.s:
+    SEND_PORT = args.s
+if args.r:
+    RECEIVE_PORT = args.r
 
-print("UDP receiving port:", UDP_PORT)
+print("UDP receiving port:", RECEIVE_PORT)
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # UDP
-sock.bind(('', int(UDP_PORT)))
+sock.bind(('', int(RECEIVE_PORT)))
 
 while True:
     message, address = sock.recvfrom(1024)
-    print(message)
-    sock.sendto(message, (IP, int(UDP_PORT)))
+    if args.v:
+        print(message)
+    sock.sendto(message, (IP, int(SEND_PORT)))
